54ec1c07dfed9784ee0ea7b0338353c915ff8d58
Tom Zhang <tom.zhang989@gmail.com>
2021-09-17 18:07:30 -0400

    completed the add everything in arrays funciton.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  console.log(array);
+  if (array.length < 1) {
+    return 0;
+  } else if (Number.isInteger(array)) {
+    return array;
+  // Just Array
+  } else if (array.length === 1) {
+    return sumItems(array[0]);
+  // Nested Array
+  } else {
+    return sumItems(array[0]) + sumItems(array.slice(1));
+  }
+
+};
 
 module.exports = sumItems;
